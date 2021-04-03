edc4ff33ca270bd870f11e537fbecb277196c9de
Bignichok <bignik545@gmail.com>
2021-04-03 11:30:29 +0300

    initial backEnd files

---
+++ index.js
@@ -0,0 +1,3 @@
+const { proTestServer } = require("./Server");
+
+proTestServer.start();
---
+++ users/user.controller.js
@@ -0,0 +1,22 @@
+const User = require("./User");
+
+class UsersController {
+    constructor() {}
+    get testController() {
+        return this._testController.bind(this);
+    }
+
+    async _testController(req, res, next) {
+        try {
+            console.log("hello");
+            res.json({
+                message: "hello this is test endPoint",
+            }).status(200);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
+
+const userController = new UsersController();
+module.exports = userController;
