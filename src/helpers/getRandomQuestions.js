faebf24bf2797683b9cd1926274696fc3e3dc30a
Vadym <vad.evlanov@gmail.com>
2021-04-07 21:04:28 +0300

    add Set

--- src/helpers/getRandomQuestions.js
+++ src/helpers/getRandomQuestions.js
@@ -1,13 +1,17 @@
 const getRandomQuestions = (data, randomFn) => {
   const uniqueSet = new Set();
-  const randomNumber = randomFn(data.length);
+  const responseData = [];
 
   do {
+    const randomNumber = randomFn(data.length);
     const { _id, question, answers } = data[randomNumber];
-    uniqueSet.add({ _id, question, answers });
+    uniqueSet.add(_id);
+    uniqueSet[_id] = { _id, question, answers };
   } while (uniqueSet.size < 12);
 
-  return [...uniqueSet];
+  uniqueSet.forEach((idx, a, set) => responseData.push(uniqueSet[idx]));
+
+  return responseData;
 };
 
 module.exports = getRandomQuestions;
